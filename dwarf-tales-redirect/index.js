5b8ba6c8e66df3c76d5fcf95ae91ff6251b2a3bf
Nicholas Westerhausen <2317381+nwesterhausen@users.noreply.github.com>
2021-05-17 10:32:23 -0400

    feat: :rocket: redirect for dwarves.nwest.wiki

---
+++ dwarf-tales-redirect/index.js
@@ -0,0 +1,15 @@
+const base = "https://dwarves.nwest.wiki"
+const statusCode = 301
+
+async function handleRequest(request) {
+  const url = new URL(request.url)
+  const { pathname, search } = url
+
+  const destinationURL = base + pathname + search
+
+  return Response.redirect(destinationURL, statusCode)
+}
+
+addEventListener("fetch", async event => {
+  event.respondWith(handleRequest(event.request))
+})
